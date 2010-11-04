8a4a3afc7f02123b0b3c85c80eb29e0e85485728
Andrew Liu <andrewliu33@gmail.com>
2010-11-04 23:40:29 +0800

    PDA fully functioned

--- pda.rb
+++ pda.rb
@@ -12,13 +12,12 @@ class PDA
   def run(input)
     current_states = expand([[@start, []]])
     for i in input
-      p current_states
       current_states = next_states(current_states, i)
       current_states = expand(current_states)
       current_states.uniq!
       return false if current_states.length == 0
     end
-    current_states.each {|s| return true if @end.include? s }
+    current_states.each {|s| return true if @end.include? s[0] }
     return false
   end
 
@@ -37,35 +36,23 @@ private
   end
 
   def next_states(states, input)
-    #p "states: ", states
     states.inject([]) do |result, state|
       if @transition[state[0]] and @transition[state[0]][input]
         next_states = @transition[state[0]][input]
         for s in next_states
-          #p "before migrate", states
           migrated = migrate state, s
-          #p "after migrate", states
           result << migrated if migrated
         end
       end
-      #p "after next states", states
       result
     end
   end
 
   def expand(states)
     new_states = states
-    #i= 0
     while true
-      #p states
       new_states = next_states(new_states, EMPTY)
-      #p new_states
-      #p states
       new_states = new_states - states
-      #p new_states
-      #p "end"
-      #return if i == 1
-      #i = i+ 1
       return states if new_states == []
       states = states + new_states
     end
@@ -80,13 +67,10 @@ if $PROGRAM_NAME == __FILE__
                 :q3 => {1 => [[0, EMPTY, :q3]], EMPTY => [['$', EMPTY, :q4]]},
                 :q4 => {}
               })
-  #p n.send :next_states, [[:q1, []]], EMPTY
-  #p n.send :next_states, [[:q2, ["$"]]], EMPTY
-  #p "expand"
-  #p n.send :expand, [[:q1, []]]
+
   puts n.run([0,0,1,1])
-#  puts n.run([0])
-#  puts n.run([1,1,0,0])
-#  puts n.run([1,1,0])
+  puts n.run([0])
+  puts n.run([0,1])
+  puts n.run([1,1,0])
 end
 
