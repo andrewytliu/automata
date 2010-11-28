add3b36d2b305d0f252992ddea637b44f95a0973
Andrew Liu <andrewliu33@gmail.com>
2010-11-28 20:12:56 +0800

    Transfer cfg to pda

--- cfg.rb
+++ cfg.rb
@@ -1,5 +1,8 @@
+require 'pda'
+
 class CFG
   attr_accessor :start, :rule
+  @@state_num = 0
 
   def initialize(options = {})
     @start = options[:start]
@@ -28,6 +31,47 @@ class CFG
       break if new_candidates.size == 0
     end
     return false
+  end
+
+  def to_pda
+    terminals = []
+    @rule.each{|k,v| v.each{|a| a.each{|s| terminals << s unless s.is_a? Symbol}}}
+    terminals.uniq!
+
+    transition = {
+      :q_start => { EMPTY => [[EMPTY, '$', :q_middle]] },
+      :q_middle => { EMPTY => [[EMPTY, @start, :q_loop]] },
+      :q_loop => { EMPTY => [['$', EMPTY, :q_accept]] },
+      :q_accept => {}
+    }
+    for terminal in terminals
+      transition[:q_loop].merge!({ terminal => [[terminal, EMPTY, :q_loop]] })
+    end
+    for k, v in @rule
+      for single in v
+        if single.size == 1
+          transition[:q_loop][EMPTY] << [k, single[0], :q_loop]
+        else
+          stack_push = single.reverse
+          next_state = new_state
+          transition[:q_loop][EMPTY] << [k, stack_push[0], next_state]
+          for s in stack_push[1..-2]
+            now_state = next_state
+            next_state = new_state
+            transition[now_state] = { EMPTY => [[EMPTY, s, next_state]] }
+          end
+          transition[next_state] = { EMPTY => [[EMPTY, stack_push[-1], :q_loop]] }
+        end
+      end
+    end
+    PDA.new :start => :q_start, :end => :q_accept, :transition => transition
+  end
+
+private
+
+  def new_state
+    @@state_num = @@state_num + 1
+    @@state_num.to_s.to_sym
   end
 end
 
